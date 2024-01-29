7cc09bdd57f98735c07dc22b4e837e1325388ebb
unknown <englishbybudi@gmail.com>
2024-01-29 14:00:38 +0700

    test123

---
+++ looping bukan tugas.js
@@ -0,0 +1,13 @@
+// let Angka_Menu=[{1:'Menampilkan daftar buah-buah'}, {2: 'Menambah buah'}, {3:'Menghapus buah'},
+// {4:'membeli buah'}, {5:'exit'}
+// ]
+
+// for(k=0; k<=1 k++){prompt(Angka_Menu[1])
+
+// }
+
+// for(let am=0; )
+
+// [0][0]
+// [1][1]
+// [2][2]
